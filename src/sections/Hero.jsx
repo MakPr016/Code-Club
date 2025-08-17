a75a9ce9cd2be52d35a37d7710fd7c4998e38af2
MakPr016 <makpr0168@gmail.com>
2025-08-18 00:35:16 +0530

    Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,13 +3,13 @@ import Button from '../components/Button'
 
 const Hero = () => {
     return (
-        <section id='hero' className='w-full h-screen'>
-            <h1 className='font-wix text-3xl font-semibold'>Build the Future,<br></br>One Step at a Time</h1>
+        <section id='hero' className='w-full h-screen flex flex-col justify-center items-center gap-10'>
+            <h1 className='font-wix text-4xl font-semibold text-center'>Build the Future,<br></br>One Step at a Time</h1>
             <p className='font-roboto text-md text-center'>Join our vibrant community of coders, where we learn, build, and innovate together. <br />
                 Explore exciting projects, attend workshops, and connect with fellow enthusiasts.</p>
             <div className="buttons-container flex gap-5">
+                <Button text='Explore Events' caps={true} />
                 <Button text='Explore Projects' caps={true} />
-                <Button text='Preview' caps={true} size='sm' className='button-sm' />
             </div>
         </section>
     )
